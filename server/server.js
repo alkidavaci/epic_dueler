a4cfe80eb88f38e5be1ef418802a8edb103d0e8e
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-03-13 20:43:53 -0400

    added changes for @Apollo/server

--- server/server.js
+++ server/server.js
@@ -1,31 +1,47 @@
 const express = require('express');
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
 const path = require('path');
-const db = require('./config/connection');
+const http = require("http");
+const cors = require("cors");
+const { json } = require("body-parser");
+
 const { typeDefs, resolvers } = require("./schemas");
+const db = require('./config/connection');
+
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   context: authMiddleware,
   persistedQueries: false,
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-  server.applyMiddleware({ app });
+  app.use(
+    '/graphql',
+    cors(),
+    json(),
+    expressMiddleware(server),
+  );
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+
+  // await server.start();
+  // server.applyMiddleware({ app });
 
   db.once('open', () => {
     app.listen(PORT, () => {
