87bb4c3548b5aa60fe5e52121ae5c0af1b8d4ac9
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-13 21:24:05 -0400

    Added Instance of ApolloClient and Entry point for app on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,3 +13,59 @@ import Login from './pages/Login';
 import NotFound from './NotFound';
 import Shop from './pages/Shop';
 import Signup from './pages/Signup';
+
+// Creates a new instance of the ApolloClient 
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
+
+// Entry point. Defines the structure and layout 
+export default function App() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+            <Header />
+            <div className="container">
+              <Routes>
+                <Route 
+                  path="/" 
+                  element={<Home />} 
+                />
+                <Route 
+                  path="/login" 
+                  element={<Login />} 
+                />
+                 <Route 
+                  path="/signup" 
+                  element={<Signup />} 
+                />
+                 <Route 
+                  path="/inventory" 
+                  element={<Inventory />} 
+                />
+                  <Route 
+                  path="/fight" 
+                  element={<Fight />} 
+                />
+                  <Route 
+                  path="/shop" 
+                  element={<Shop />} 
+                />
+                   <Route 
+                  path="/battle" 
+                  element={<Battle />} 
+                />
+                    <Route 
+                  path="*" 
+                  element={<NotFound />} 
+                />
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
