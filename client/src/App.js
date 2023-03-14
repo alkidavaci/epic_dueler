f46e56d4a6e883b5ddad78f9dfee7070f35204fc
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-13 21:15:43 -0400

    Import statments to App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// Import statement of three modules from the @apollo/client package
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+// Import statement of three modules from the react-router-dom package for client-side routing
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import component
+import Battle from './pages/Battle';
+import Footer from './Footer';
+import Home from './pages/Home';
+import Inventory from './pages/Inventory';
+import Fight from './pages/Fight';
+import Login from './pages/Login';
+import NotFound from './NotFound';
+import Shop from './pages/Shop';
+import Signup from './pages/Signup';
