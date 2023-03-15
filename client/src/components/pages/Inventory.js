0838d570c9c1786d1fe443b18905810904f10c5e
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-15 12:05:40 -0400

    Added Inventory components

---
+++ client/src/components/pages/Inventory.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../../utils/gql/queries";
+
+const InventoryBar = () => {
+  const { loading, data } = useQuery(QUERY_ME);
+  const profiles = data?.profiles || [];
+  const inventory = profiles.inventory || [];
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      {inventory.length > 0 &&
+        inventory.map((elem) => (
+          <div className="dropdown is-active" key={elem}>
+            <div className="dropdown-trigger">
+              <button
+                className="button"
+                aria-haspopup="true"
+                aria-controls="dropdown-menu"
+              >
+                <span>{elem}</span>
+              </button>
+            </div>
+            <div className="dropdown-menu" id="dropdown-menu" role="menu">
+              <div className="dropdown-content">
+                {elem.items &&
+                  elem.items.map((item) => (
+                    <a href="#" className="dropdown-item" key={item.id}>
+                      {item.name}
+                    </a>
+                  ))}
+              </div>
+            </div>
+          </div>
+        ))}
+    </>
+  );
+};
+
+export default InventoryBar;
