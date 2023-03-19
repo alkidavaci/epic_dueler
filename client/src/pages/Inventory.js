a73f7accac952b98f1a157e22c011951cd307b41
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-18 23:18:37 -0400

    Created a usercharater component>

--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../utils/gql/queries";
-import { InventoryList } from "../components";
+import { InventoryList, UserCharacter } from "../components";
 // const [selectedItem, setSelectedItem] = useState({
 //   item: {
 //     _id: '',
@@ -16,7 +16,7 @@ import { InventoryList } from "../components";
 const Inventory = () => {
   const { loading, data, error } = useQuery(QUERY_ME);
   const inventory = data?.me?.inventory || [];
-  console.log(inventory)
+  console.log(inventory.bag)
 
   // Dropdown active onclick
   const [activeDropdown, setActiveDropdown] = useState(null);
@@ -24,6 +24,13 @@ const Inventory = () => {
     setActiveDropdown((prevKey) => (prevKey === key ? null : key));
   };
 
+  // Handle item selection
+  const [selectedItem, setSelectedItem] = useState({
+
+  });
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
   // loading and errors
   if (loading) {
     return <div>Loading...</div>;
@@ -38,53 +45,60 @@ const Inventory = () => {
     if (key === "weapon") {
       return (`Weapon: ${inventory.weapon.name}`)
     }
-    else if (key === "armor"){
+    else if (key === "armor") {
       return (`Armor: ${inventory.armor.name}`)
     }
-    else if (key === "slot1"){
+    else if (key === "slot1") {
       return (`Slot1: ${inventory.slot1.name}`)
     }
-    else if (key === "slot2"){
+    else if (key === "slot2") {
       return (`Slot2: ${inventory.slot2.name}`)
     }
-    else if (key === "slot3"){
+    else if (key === "slot3") {
       return (`Slot3: ${inventory.slot3.name}`)
     }
-    else if (key === "slot4"){
+    else if (key === "slot4") {
       return (`Slot4: ${inventory.slot4.name}`)
     }
   }
 
   return (
     <div>
-    {Object.keys(inventory).map((key, index) => {
-      if (index > 0 && index < 7) {
-        return (
-          <div className={`dropdown ${activeDropdown === key ? 'is-active' : ''}`} key={key._id}>
-            <div className="dropdown-trigger">
-              <button className="button" onClick={() => handleDropdownClick(key)}>
-                <span>{callKey(key)}</span>
-                <span className="icon is-small">
-                  <i className="fas fa-angle-down" aria-hidden="true"></i>
-                </span>
-              </button>
-            </div>
-            <div className="dropdown-menu" id="dropdown-menu" role="menu">
-              <div className="dropdown-content">
-                {/* {inventory[key].map((item) => (
-                  <a href="#" className="dropdown-item" key={item._id}>
-                    <img src={item.icon} alt={item.name} width="32" height="32" /> {item.name}
-                  </a>
-                ))} */}
+      {Object.keys(inventory).map((key, index) => {
+        if (index > 0 && index < 7) {
+          return (
+            <div className={`dropdown ${activeDropdown === key ? 'is-active' : ''}`} key={key._id}>
+              <div className="dropdown-trigger">
+                <button className="button" onClick={() => handleDropdownClick(key)}>
+                  <span>{callKey(key)}</span>
+                  <span className="icon is-small">
+                    <i className="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </div>
+              <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                <div className="dropdown-content">
+                  {inventory.bag.map((item, i) => {
+                    if (inventory.bag[i].itemtype === key) {
+                      return (
+                        <a href="#" className="dropdown-item" key={item.name} onClick={() => handleItemClick(item)}>
+                          {item.icon}  {item.name}
+                        </a>
+                      );
+                    } else {
+                      return null;
+                    }
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-        );
-      }
-      return null; 
-    })}
-    {/* <div><InventoryList inventory={inventory} /></div> */}
-  </div>
+          );
+        }
+        return null;
+      })}
+      <UserCharacter Item = {selectedItem}  />
+      {/* <div><InventoryList inventory={inventory} /></div> */}
+    </div>
   );
 }
 
