4ce87fd6dd699c2d64e170b7a3d73b63f5e02255
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-19 20:09:46 -0400

    Reload page to render navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import Auth from '../../utils/Auth';
 import Nav from '../Nav'
-import Inventory from '../../pages/Inventory';
-import Button from 'react-bootstrap/Button';
 
 function Header() {
   // Set isLoggedIn to true or false based on whether the user is logged in
@@ -30,11 +28,12 @@ function Header() {
   };
 
   return (
-    <div className="header"  >
+    <div>
+    {isLoggedIn ? (<div className="header"  >
       <nav className="navbar is-primary" style={{ boxShadow: ' 0 0 8px #999' }} role="navigation" aria-label="main navigation" >
 
         <h1 className="title" style={{ fontSize: '50px', left: '963px', position: 'absolute', padding: '25px', top: '15px' }}>Epic Dueler</h1>
-        {isLoggedIn && (
+        
           <div className=''>
 
             <div className="navbar-item" style={{ top: '10px', color: 'black', fontSize: '40px', fontFamily: 'monospace' }}>
@@ -51,8 +50,18 @@ function Header() {
               {/* <Inventory /> */}
             </div>
           </div>
+          </nav>
+          </div>
+        ) : ( 
+          <div>
+
+          <nav className="navbar is-primary" style={{ boxShadow: ' 0 0 8px #999' }} role="navigation" aria-label="main navigation" >
+
+          <h1 className="title" style={{ fontSize: '50px', left: '963px', padding: '25px', top: '15px' }}>Epic Dueler</h1>
+          </nav>
+          </div>
         )}
-      </nav>
+      
     </div>
 
   );
